a3899dbf70d4d16d289c4942bcf5ef7ec24bb2ac
sosama-nisum-com <sosama@nisum.com>
2020-12-18 18:01:54 +0500

    Dashboard complete.

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,6 +4,7 @@ import { Layout, Row, Col, Button } from 'antd';
 import SelectComp from './common/select';
 import InputComp from './common/input';
 import BaseForm from "./common/form";
+import DisplayModal from "./displayModal";
 import { getUser } from "../services/auth";
 import { Redirect } from "react-router";
 
@@ -14,18 +15,53 @@ const Dashboard = () => {
     const user = getUser()
     if(!user) return <Redirect to="/login"/>
 
-    let data = {}
+    const [title, setTitle] = useState("");
+
+    const [data,setData] = useState({});
+
     const [fieldDetails,setFieldDetails] = useState([]);
 
     const [previewDisabled,setPreviewDisabled] = useState(true);
 
-    const handleChange = ({currentTarget: input}) => {
+    const [editMode,setEditmode] = useState(true);
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const doSubmit = (values) =>{
+        const fieldData = {}
+        for(let i=0;i<fieldDetails.length;i++){
+            if(fieldDetails[i].type === "checkbox" && !values[fieldDetails[i].name ]){
+                fieldData[fieldDetails[i].label] = false;    
+            }else{
+                fieldData[fieldDetails[i].label] = values[fieldDetails[i].name];
+            }
+        }
+        setData(fieldData)
+        setIsModalVisible(true)
+    }
+
+    const handleChangeTitle = ({currentTarget: input}) => {
+        setTitle(input.value);
         if(!input.value) {
             setPreviewDisabled(true)
             return
         }
 
         setPreviewDisabled(false)
+    }
+
+    const handleChangeFields = (value) => {
+        if(value.target && value.target.type === "checkbox"){
+            value.target.value = value.target.checked
+        }
     }
 
     const handleChangeSelect = (value) =>{
@@ -38,26 +74,33 @@ const Dashboard = () => {
         let newField = {name: `question${fieldDetails.length+1}`, label:`Question ${fieldDetails.length+1}`, required: false, placeholder: "", type: value.toLowerCase()}
         
         const newFieldData = [...fieldDetails,newField]; 
-        
         setFieldDetails(newFieldData)
     }
+
+    const handleClick = () =>{
+        setEditmode(false)
+    }
+
+    const height = fieldDetails.length > 2 ? "auto": "273px";
+
     return(<div>
+        <DisplayModal title={title} data={data} isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel}/>
         <NavBar/>
 
         <Layout.Content style={{marginTop:"2%"}} >
             <Row >
             <Col xs={20} sm={15} lg={7} xl={1}></Col>
-            <Col id="login" xs={20} sm={15} lg={7} xl={7} >
-                <SelectComp onChange={handleChangeSelect} options={options}/>
-                <Button style={{marginLeft:"5px"}} disabled={previewDisabled} type="primary" htmlType="button">
+            <Col id="dashboard" xs={20} sm={15} lg={7} xl={7} >
+            {editMode &&  <React.Fragment>  <SelectComp onChange={handleChangeSelect} options={options}/>
+                <Button onClick={handleClick} style={{marginLeft:"5px"}} disabled={previewDisabled} type="primary" htmlType="button">
                 Preview
-                </Button>
+                </Button></React.Fragment>}
                 <div style={{marginTop:"10%"}}>
-                    <InputComp name={"title"} label={""} required={true} placeholder={"Enter Form Title"} type={"text"} onChange={handleChange}/>
+                    <InputComp name={"title"} label={""} required={true} placeholder={"Enter Form Title"} type={"text"} onChange={handleChangeTitle}/>
                 </div>
-                <div style={{border:"1px solid #d9d9d9",marginTop: "-25px", height: "273px"}}>
+                <div style={{border:"1px solid #d9d9d9",marginTop: "-25px", height: height}}>
                     <div style={{marginLeft:"10%",marginTop:"5%"}}>
-                    <BaseForm  data={data} fieldDetails={fieldDetails} submitable={false} btnText={"login >"}/>
+                    <BaseForm doSubmit={doSubmit} handleChange={handleChangeFields} data={data} fieldDetails={fieldDetails} submitable={!editMode} btnText={"Submit"}/>
                     </div>
                 </div>
             </Col>
