6714c685c83223a56bbbbf1883f7bc316c16e152
sosama-nisum-com <sosama@nisum.com>
2020-12-21 19:54:39 +0500

    Add drag functionality.

---
+++ src/Constants.js
@@ -0,0 +1,5 @@
+export const ItemTypes = {
+    SELECT: 'select',
+    TEXT: 'text',
+    CHECKBOX: 'checkbox'
+  }
--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Layout, Menu, Button } from 'antd';
+import { Layout, Menu, Button} from 'antd';
 const { Sider } = Layout;
+import { ItemTypes } from '../Constants';
+import { useDrag } from 'react-dnd'
+
+const SideBar = () => {
+
+    const [{isDraggingSelect}, dragSelect] = useDrag({
+        item: { type: ItemTypes.SELECT },
+        collect: monitor => ({
+        isDragging: !!monitor.isDragging(),
+        }),
+  })
+
+    const [{isDraggingText}, dragText] = useDrag({
+        item: { type: ItemTypes.TEXT },
+        collect: monitor => ({
+        isDragging: !!monitor.isDragging(),
+        }),
+    })
+
+    const [{isDraggingCheckbox}, dragCheckbox] = useDrag({
+        item: { type: ItemTypes.CHECKBOX },
+        collect: monitor => ({
+        isDragging: !!monitor.isDragging(),
+        }),
+    })
 
 
-const SideBar = ({ handleClick }) => {
     return(
         <Sider className="site-layout-background" id="sidebar" style={{marginTop:"50px"}}>
         <Menu
@@ -14,17 +37,12 @@ const SideBar = ({ handleClick }) => {
           style={{ height: '100%', borderRight: 0 }}
         >
             <Menu.Item ><h2 style={{cursor:"default"}}>Fields</h2></Menu.Item>
-            <Menu.Item key="2"><Button onClick={() => handleClick("select")}>Dropdown</Button></Menu.Item>
-            <Menu.Item key="3"><Button onClick={() => handleClick("text")} style={{width:"97.6px"}}>Text</Button></Menu.Item>
-            <Menu.Item key="4"><Button onClick={() => handleClick("checkbox")} style={{width:"97.6px"}}>Checkbox</Button></Menu.Item>
+            <Menu.Item key="2"><Button ref={dragSelect} style={{opacity: isDraggingSelect ? 0.5 : 1,cursor: 'move'}}>Dropdown</Button></Menu.Item>
+            <Menu.Item key="3"><Button ref={dragText} style={{opacity: isDraggingText ? 0.5 : 1,cursor: 'move',width:"97.6px"}}>Text</Button></Menu.Item>
+            <Menu.Item key="4"><Button ref={dragCheckbox} style={{opacity: isDraggingCheckbox ? 0.5 : 1,cursor: 'move',width:"97.6px"}}>Checkbox</Button></Menu.Item>
         </Menu>
       </Sider>
     )
 }
-
-SideBar.propTypes = {
-    handleClick: PropTypes.func.isRequired
-}
-
 
 export default SideBar;
